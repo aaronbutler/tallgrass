3b294c9a37e60e2b663f1f8f93d69d42c1f34b86
aaron butler <aaron butler>
2015-11-25 10:13:37 -0500

    add index2.html which uses promises api and css transitions

--- src/js/util.js
+++ src/js/util.js
@@ -29,54 +29,6 @@ Log.prototype.log = function(level, objname, methodname, message, object) {
 };
 
 /**
-*A utility to manage events which depend on multiple asynchronous events
-*@this {CoincidenceCallbackRegistry}
-*@param {none}
-*/
-var CoincidenceCallbackRegistry = function () {
-	this.registry = {};
-};
-
-/**
-*Register an event as dependent upon multiple asynch events
-*@this {CoincidenceCallbackRegistry}
-*@param {string} name the identifier for the thing which must happen after other things
-*@param {array} reqs the array of strings identifying the asynchronous events which must happen first
-*@param {function} callback the callback function which must be run after the reqs have completed
-*/
-CoincidenceCallbackRegistry.prototype.register = function(name, reqs, callback) {
-	var ONAME = 'CoincidenceCallbackRegistry';
-	var FNAME = 'register';
-	
-	this.registry[name] = {reqs: reqs,callback: callback};
-	log.log(3,ONAME,FNAME,'registering a callback',this.registry);
-};
-
-/**
-*Removes a completed event from the registry reqs list
-*@this {CoincidenceCallbackRegistry}
-*@param {string} name the key in the registry which identifies  the thing which must happen after other things 
-*@param {string} member the name of the req event which has completed
-*/
-CoincidenceCallbackRegistry.prototype.memberReady = function(name, member) {
-	var ONAME = 'CoincidenceCallbackRegistry';
-	var FNAME = 'memberReady';
-	log.log(3,ONAME,FNAME,member+' is ready for '+name,this.registry);
-	var entry = this.registry[name];
-	if(entry != null) {
-		var index = entry.reqs.indexOf(member);
-		if(index > -1) {
-			entry.reqs.splice(index,1);
-			if (entry.reqs.length == 0) {
-				entry.callback();
-				//this.registry.splice(i,1);
-				this.registry[name] = null;
-			}
-		}
-	}
-};
-
-/**
 *Escapes characters which are reserved in html
 *@author stolen from http://stackoverflow.com/questions/24816/escaping-html-strings-with-jquery
 *@param string the string whose characters should be made html-safe
