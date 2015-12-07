bd00bd320df110bde1168e1dd59876d78ac8717b
aaron butler <aaron butler>
2015-12-07 13:54:48 -0500

    remove unnecessary whitespace chars from app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@
 $(document).ready(function () {
 	viewModel = new MarsViewModel();
 	ko.applyBindings(viewModel);
-	
+
 	$('.markerTitles').click(function(){$('#MapSearchSection').toggleClass('hide');});
 });
 
@@ -15,17 +15,16 @@ $(document).ready(function () {
 */
 function MarsViewModel() {
 	var self = this;
-	
+
 	//appModel
 	var d = new Date();
 	d.setFullYear(2012, 7, 6);
 	self.solendar = new Solendar(0,d);
 	self.sols = [];
-	
+
 	self.searchQuery = ko.observable();
 	self.solNumber = ko.observable();
-	
-	
+
 	//mapModel
 	self.map = '';
 	self.points = [];
@@ -34,41 +33,41 @@ function MarsViewModel() {
 	self.subLine = null;
 
 	self.landmarks = ko.observableArray();
-	
+
 	//solModel
 	self.currentPicArray = ko.observableArray();
 	self.currentPicID = ko.observable();
 	self.solPicError = ko.observable();
-	
+
 	self.currentWeatherData = ko.observable();
 	self.solWeatherError = ko.observable();
-	
+
 	self.solthText = ko.computed(function() {
 		var _s = self.solendar;
 		var solth = self.solNumber();
 		var d = _s.SolToDate(solth);
 		return 'On the '+_s.Solth(solth) + ' sol ('+ d.toLocaleDateString()+'):';
 	},this);
-	
+
 	self.picText = ko.computed(function() {
 		if(self.currentPicArray() == null || self.currentPicArray().length === 0) {
 			return "No pictures available on this sol";
 		}
 		else {return 'Picture '+(self.currentPicID() + 1) + ' of '+ self.currentPicArray().length;}
 	},this);
-	
+
 	//infoModel
 	self.wikiTimeMars = ko.observable();
 	self.wikiTimeError = ko.observable();
 	self.wikiCuriosityRover = ko.observable();
 	self.wikiCuriosityError = ko.observable();
-	
+
 	//Behaviors//////////////////////////////
-	
+
 	self.goToMap = function() { location.hash = 'mmap/'; };
 	self.goToInfo = function() { location.hash = 'info/';};
 	self.goToSol = function(sol) { location.hash = 'sol/' + sol; };
-	
+
 	self.searchSol = function(solnum) {
 		var ONAME = 'MarsViewModel';
 		var FNAME = 'searchSol';
@@ -85,7 +84,7 @@ function MarsViewModel() {
 					self.solPicError("Sorry, couldn't retrieve Curiosity's pictures. Try again later");
 				}
 				else {self.solPicError(null);}
-				
+
 				if(arrayOfResults[2] === false){
 					self.solWeatherError("Sorry, couldn't retrieve Curiosity's weather. Try again later");
 				}
@@ -93,19 +92,18 @@ function MarsViewModel() {
 					self.solWeatherError("No weather data available for this sol.");
 				}
 				else {self.solWeatherError(null);}
-				
+
 				log.log(3,ONAME,FNAME,'solPromises kept');
 				self.subLine = arrayOfResults[0];
 				self.currentPicID(0);
 				$('#SolSection').removeClass('hide');
 
-			
 			}, function(e){
 				log.log(3,ONAME,FNAME,'Something went wrong...',e);
 			});
 		});
 	};
-	
+
 	self.search = function(enter) {
 
 		var q = cleanse(self.searchQuery());
@@ -114,7 +112,7 @@ function MarsViewModel() {
 			showAllLandmarks(self.landmarks());
 			return true;
 		}
-		
+
 		if(q.startsWith(':')) {
 			q=$.trim(q.slice(1));
 			self.solNumber(q);
@@ -139,19 +137,19 @@ function MarsViewModel() {
 		}
 		return true;
 	};
-	
+
 	self.clickLandmark = function(d,e) {
 		d.mapMarker.clicker();
 	};
-	
+
 	self.prevPic = function(d,e) {
 		self.currentPicID(self.currentPicID()-1);
 	};
-	
+
 	self.nextPic = function(d,e) {
 		self.currentPicID(self.currentPicID()+1);
 	};
-	
+
 	//Initializers///////////////////////////
 	var locationPromise = getLocations();
 	self.mapPromise = buildMap();
@@ -159,7 +157,7 @@ function MarsViewModel() {
 		return new Promise(function(resolve,reject){
 			self.sols = arrayOfResults[0];
 			self.map = arrayOfResults[1];
-			
+
 			self.points = makePointsArray(self.sols);
 			self.line = buildPath(self.map, self.points);
 			self.line.icons = null;
@@ -168,7 +166,7 @@ function MarsViewModel() {
 			resolve(true);
 		});
 	});
-	
+
 	self.landmarks(initLandmarks());
 	self.mapPromise.then(function(response){
 		var ONAME = 'MarsViewModel';
@@ -181,24 +179,24 @@ function MarsViewModel() {
 	
 	populateMarsTimeData(self.wikiTimeMars, self.wikiTimeError);
 	populateCuriosityData(self.wikiCuriosityRover, self.wikiCuriosityError);
-	
+
 	//client-side routes
-	
+
 	$('.solCloser').click(function(){
 		self.goToMap();
 	});
-	
+
 	$('.infoCloser').click(function(){
 		self.goToMap();
 	});
 	$('.infoIcon').click(function(){
 		self.goToInfo();
 	});
-	
+
 	$('.searchIcon').click(function(){
 		$('#MapSearchSection').toggleClass('hide');
 	});
-	
+
 	Sammy(function() {
 
 		this.get('#mmap/', function() {
@@ -208,7 +206,7 @@ function MarsViewModel() {
 			self.searchQuery('');
 
 		});
-		
+
 		this.get('#info/', function() {
 
 			$('#InfoSection').removeClass('hide');
@@ -220,7 +218,7 @@ function MarsViewModel() {
 
 			if(self.solNumber() !== sol) {
 				self.solNumber(sol);
-				
+
 			}
 			self.searchQuery(':'+sol);
 
@@ -231,8 +229,7 @@ function MarsViewModel() {
 		});
 		this.get('', function() { this.app.runRoute('get', '#mmap'); });
 	}).run(); 
-	
-	
+
 };
 
 /**
@@ -256,7 +253,7 @@ function cleanse(query) {
 *@return the promise which resolves with the sols array
 */
 function getLocations() {
-	
+
 	var ONAME =  'Global';
 	var FNAME = 'getLocations';
 	log.log(3,ONAME,FNAME,'entering getLocations',this);
@@ -282,7 +279,7 @@ function buildMap() {
 	var FNAME = 'buildMap';
 	log.log(3,ONAME,FNAME,'entering buildMap',this);
 	return new Promise(function(resolve,reject){
-	
+
 		var mq = window.matchMedia( "(max-width: 768px)" );
 		var z=mq.matches?11:13;
 		var latLng = new google.maps.LatLng(-4.63,137.395);
@@ -294,7 +291,7 @@ function buildMap() {
 		var el = document.getElementById('map');
 		var map = new google.maps.Map(el, mapOptions);
 		map.cent = latLng;
-		
+
 		var quadLayer = new google.maps.KmlLayer({
 			//url: 'http://aaronbutler.github.io/kmls/quadrants.kml',
 			//url: 'http://aaronbutler.github.io/marsmap/kml/quadrants.kml',
@@ -303,8 +300,7 @@ function buildMap() {
 			map: map
 		});
 		resolve(map);
-	
-	
+
 	});
 };
 
@@ -399,11 +395,11 @@ function fullyAnimateCircle(sols, map) {
 function populatePicData(solNumber, picArray) {
 	var ONAME = 'picture promise';
 	var FNAME = 'populatePicData';
-	
+
 	return new Promise(function(resolve, reject) {
 		var picRequestTimeout = setTimeout(function(){resolve(false);},8000);
 		var picLink = 'http://msl-raws.s3.amazonaws.com/images/images_sol'+solNumber+'.json';
-		
+
 		$.ajax({
 
 			  type: 'GET',
@@ -416,27 +412,27 @@ function populatePicData(solNumber, picArray) {
 			  success: function(result) {
 				var pa = solPicArray(result);
 				log.log(3,'pa','solPicArray','what came back from solPicArray',pa);
-				
+
 				if(pa.length >= 0) {
 
 					picArray(pa);
-					
+
 				}
 				else {
 
 					picArray(null);
-					
+
 				}
 				log.log(3,ONAME,FNAME,'after populating currentpicarray');
 				clearTimeout(picRequestTimeout);
 				resolve(true);
-			  },
-			  
-			  error: function(e) {
+			},
+
+			error: function(e) {
 				log.log(3,ONAME,FNAME,'failed to populate picture data',e);
 				resolve(false);
 
-			  }
+			}
 		});
 
 	});
@@ -455,7 +451,6 @@ function populateWeatherData(solNumber, weatherData) {
 		var ONAME = 'Weather promise';
 		var FNAME = 'populateWeatherData';
 
-	
 		var weatherLink = 'http://marsweather.ingenology.com/v1/archive/?format=jsonp&sol='+solNumber;
 		var weatherRequestTimeout = setTimeout(function(){resolve(false);},8000);
 		$.ajax({
@@ -469,14 +464,13 @@ function populateWeatherData(solNumber, weatherData) {
 				clearTimeout(weatherRequestTimeout);
 				resolve(true);
 
-				
 			},
 			
 			error: function(e) {
 				log.log(3,ONAME,FNAME,'failed to populate weather data',e);
 				resolve(false);
 
-			  }
+			}
 		});
 
 	});
@@ -492,27 +486,26 @@ function initLandmarks() {
 	var ONAME = 'global';
 	var FNAME = 'initLandmarks';
 	log.log(1,ONAME,FNAME,'Entering initLandmarks');
-	
+
 	var landmarks = [];
 	landmarks.push(new Landmark('Yellowknife Bay Quadrant','https://en.wikipedia.org/wiki/Yellowknife_Bay,_Mars',"Yellowknife Bay is a geologic formation in Gale Crater on the planet Mars. NASA's Mars Science Laboratory Rover, named Curiosity, arrived at the low lying depression on December 17, 2012, 125 sols, or martian days, into its 668 sol planned mission on the planet. Primary mission goals of the Mars Science Laboratory were to assess the potential habitability of the planet and whether or not the Martian environment is, or has ever been, capable of supporting life. The site was chosen after much study of the region by previous missions. The Mars Reconnaissance Orbiter observed morphological features created by the presence of liquid water, suggesting the presence of an ancient lake which could have sustained microbial life. The geologic depression takes its name from the city Yellowknife, capital of the Canadian Northwest Territories, in honor of the 4 billion year old rock in the region surrounding the city, which matches the approximate age of the uncovered rock in Gale Crater.",137.445,-4.58));
-	
+
 	landmarks.push(new Landmark('Mawson Quadrant','?','Martian rollover quadrant? Not much seemed to happen here.',137.445,-4.605));
-	
+
 	landmarks.push(new Landmark('Coeymans Quadrant','?','Martian rollover quadrant? Not much seemed to happen here.',137.42,-4.605));
-	
+
 	landmarks.push(new Landmark('Kimberley Quadrant','http://www.jpl.nasa.gov/news/news.php?feature=4100','On Wednesday, NASAs Curiosity Mars rover drove the last 98 feet feet (30 meters) needed to arrive at a site planned since early 2013 as a destination for studying rock clues about ancient environments that may have been favorable for life.The rover reached a vantage point for its cameras to survey four different types of rock intersecting in an area called "the Kimberley," after a region of western Australia."This is the spot on the map weve been headed for, on a little rise that gives us a great view for context imaging of the outcrops at the Kimberley," said Melissa Rice of the California Institute of Technology, Pasadena. Rice is the science planning lead for what are expected to be several weeks of observations, sample-drilling and onboard laboratory analysis of the areas rocks.',137.42,-4.63));
-	
+
 	landmarks.push(new Landmark('Hanover Quadrant','http://mars.jpl.nasa.gov/msl/multimedia/images/?ImageID=6354','This map shows in red the route driven by NASAs Curiosity Mars rover from the "Bradbury Landing" location where it touched down in August 2012 (blue star at upper right) through the 663rd Martian day, or sol, of the rovers work on Mars (June 18, 2014). The white line shows the planned route ahead to reach "Murray Buttes" (at white star), the chosen access point to destinations on Mount Sharp. The rover will complete a mission goal of working for a full Martian year on Sol 669 (June 24, 2014). A Martian year is 687 Earth days. Gridlines indicate quadrants charted before the rovers landing for purposes of geological mapping of the landing region within Mars Gale Crater. The Sol 663 location is within the Hanover quadrant. Next on the rovers route is the Shoshone quadrant.',137.395,-4.63 ));
-	
+
 	landmarks.push(new Landmark('Shoshone Quadrant','http://astrogeology.usgs.gov/news/astrogeology/sol-671-update-on-curiosity-from-usgs-scientist-ryan-anderson-long-drive',' After a 107 m drive on sol 670, we are now in Shoshone quad, and just 160 m from the edge of the landing ellipse! The sol 671 plan is a lot like the sol 670 plan, with a 3 hour drive as the main activity. These long drives often use visual odometry, where the rover takes pictures along the way to monitor how the drive is going and avoid obstacles. This is a great capability, allowing us to drive farther than we could otherwise, but a side effect is that it produces a lot of data. The result: less data available for science observations.All of which is to say that today was another data-constrained sol. There’s always a way to squeeze some science in though! Today’s plan has a color stereo image of a rock dubbed Lost Burro, a ChemCam passive observation of the sky, and a NavCam movie of the sky looking for clouds. (Passive means that we don’t fire the laser, we just passively collect the spectrum of the target.) We also managed to squeeze a ChemCam measurement of our titanium calibration target and a MAHLI end-of-drive stowed image between the orbiter communication passes. And of course, we always do routine environmental monitoring with RAD, REMS, and DAN. Plus, after each drive we take clast survey images of the ground with Mastcam. Not bad for a data-constrained sol! ',137.395,-4.655));
-	
+
 	landmarks.push(new Landmark('Arlee Quadrant','http://missoulian.com/news/local/nasa-comes-to-arlee-the-martian-one/article_382792c8-19fd-5320-b1ad-2769c8b5fa53.html','The Mars rover Curiosity was rolling through Arlee on Friday. Not that Arlee. The one on Mars.“Because western Montana is interesting geologically, it ranked up there to get on the names list,” said Brian Nixon, who operates the Rover’s cameras for Malin Space Science Systems in San Diego. “Those are the formal names we use among team members to reference certain features. "A little while ago, the rover was going up something we called Logan Pass. That turned out to be too difficult to drive up, so I suggested the next gap over, which we called Marias Pass. If you’re going through Glacier (National Park) and you can’t make it up, you go south and go through the year-round pass.”',137.37,-4.655));
-	
+
 	landmarks.push(new Landmark('Windhoek Quadrant','http://www.az.com.na/lokales/windhoek-quadrat-auf-dem-mars.425665','Der jahrelange Leiter der Wissenschaftlergruppe, die mit dem Fahrzeug Curiosity den Mars erforscht, weilte wieder einmal in Namibia und findet die Naukluft ebenso interessant wie den Roten Planeten. Bald soll sich das sechsrädrige Forschungslabor in ein Quadrat mit namibischem Namen begeben. Dr. John Grotzinger hielt in Windhoek einen Vortrag und ist Empfänger der Henno-Martin-Medaille.',137.37,-4.68));
-	
+
 	landmarks.push(new Landmark('Bar Harbour Quadrant','Captain Jean-Luc Picard','Five card stud, nothing wild, and the sky is the limit.',137.345,-4.68));
-	
-	
+
 	return landmarks;
 };
 
@@ -537,7 +530,7 @@ function addLandmark(map, landmark) {
 			this.infowindow.open(this.map, this);
 		}
 	});
-	
+
 	var contentString = '<h2>'+landmark.name+'</h2><p>'+landmark.description+'</p><h3>From: '+landmark.credits+'</h3>';
 	var infowindow = new google.maps.InfoWindow({content: contentString});
 
@@ -547,12 +540,12 @@ function addLandmark(map, landmark) {
 		map.setCenter(map.cent);
 		infowindow.close();
 	});
-	
+
 	marker.addListener('click', function(){
 		marker.setAnimation(google.maps.Animation.DROP);
 		infowindow.open(map,marker);
 	});
-	
+
 	marker.setMap(map);
 
 	return marker;
@@ -587,10 +580,10 @@ function filterLandmarks(landmarks, q) {
 */
 function showAllLandmarks(landmarks) {
 	landmarks.forEach(function(landmark){
-		
+
 			landmark.showme(true);
 			landmark.mapMarker.setVisible(true);
-		
+
 	});
 };
 
@@ -658,7 +651,6 @@ function populateMarsTimeData(wikiMarsTime, timeError) {
 		  }
 	});
 
-	
 };
 
 /**
@@ -677,13 +669,12 @@ function populateCuriosityData(wikiCuriosity, curiosityError) {
 		type: 'GET',
 		dataType: 'jsonp',
 		url: wikiApiLink,
-		
+
 		error: function(jqxhr, textstatus, e) {
 			log.log(3,ONAME,FNAME,'failed to populate curiosity data',jqxhr);
-			
+
 
 		},
-		 
 		success: function(result) {	
 
 			var pages = result.query.pages;
@@ -695,9 +686,8 @@ function populateCuriosityData(wikiCuriosity, curiosityError) {
 			wikiCuriosity(extract);
 
 		}
-		
-		
+
 	});
 
-	
+
 };
